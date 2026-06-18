2f49fb350a723abe839e35fd96aa5702fbdd8d98
arambarzani07 <arambarzani070@gmail.com>
2026-06-18 13:40:13 +0300

    Expand payment preview text helper

--- lib/utils/payment_preview_text.dart
+++ lib/utils/payment_preview_text.dart
@@ -3,6 +3,22 @@ import 'package:zhirox/utils/constants.dart';
 class PaymentPreviewText {
   const PaymentPreviewText._();
 
+  static String titleForCustomer(String customerName) {
+    return '${AppStrings.paymentForCustomer} $customerName';
+  }
+
+  static String amountLabel() {
+    return AppStrings.paymentAmount;
+  }
+
+  static String bulkNote() {
+    return AppStrings.bulkPaymentNote;
+  }
+
+  static String savedForDebtCount(int count) {
+    return '${AppStrings.paymentSaved} ($count ${AppStrings.debt})';
+  }
+
   static String fullyReceivedDebtLabel(int count) {
     return '✓ $count ${AppStrings.paid}';
   }
