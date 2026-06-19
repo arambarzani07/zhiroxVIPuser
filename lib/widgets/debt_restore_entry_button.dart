6ad08490c7d943f0c63db5586d388c8e1c625faf
arambarzani07 <arambarzani070@gmail.com>
2026-06-19 09:19:38 +0300

    Add reusable restore entry button

---
+++ lib/widgets/debt_restore_entry_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zhirox/providers/auth_provider.dart';
+import 'package:zhirox/screens/shared/debt_restore_screen.dart';
+import 'package:zhirox/utils/constants.dart';
+import 'package:zhirox/utils/helpers.dart';
+
+class DebtRestoreEntryButton extends StatelessWidget {
+  final Future<void> Function()? onReturn;
+  const DebtRestoreEntryButton({super.key, this.onReturn});
+
+  Future<void> open(BuildContext context) async {
+    final auth = context.read<AuthProvider>();
+    if (!auth.isManager) {
+      AppHelpers.showSnackBar(context, AppUserMessages.needsManagerApproval, isError: true);
+      return;
+    }
+    await Navigator.push(context, MaterialPageRoute(builder: (_) => const DebtRestoreScreen()));
+    if (onReturn != null) await onReturn!();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton.icon(
+      onPressed: () => open(context),
+      icon: const Icon(Icons.restore_rounded, color: AppColors.primary),
+      label: const Text('گەڕاندنەوەی کردار'),
+    );
+  }
+}
