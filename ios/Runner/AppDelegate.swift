e4b6614d1c4657da0587ffb14bdd688449455fdc
arambarzani07 <arambarzani070@gmail.com>
2026-09-09 23:14:29 +0300

    Fix iOS startup crash with UIScene Workmanager registration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,26 @@
 import Flutter
 import UIKit
+import workmanager_apple
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    // iOS/UIScene requires Workmanager launch handlers to be registered
+    // before application launch finishes, otherwise BGTaskScheduler can
+    // terminate the app with NSInternalInconsistencyException.
+    WorkmanagerPlugin.registerLaunchHandlers()
+
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
