98f15d1284c89dbc4ac3ba40d47913a9646ad3e5
arambarzani07 <arambarzani070@gmail.com>
2026-09-10 07:59:08 +0300

    Replace default counter test with Zhirox configuration smoke tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:zhirox/main.dart';
+import 'package:zhirox/utils/constants.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ZhiroxApp());
+  group('Zhirox configuration', () {
+    test('uses the expected Supabase project', () {
+      expect(SupabaseConfig.url, startsWith('https://'));
+      expect(SupabaseConfig.url, contains('zfuvnczdqifihsnrhqzj'));
+      expect(SupabaseConfig.publishableKey, isNotEmpty);
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    test('has a non-empty application version', () {
+      expect(AppConfig.appVersion, isNotEmpty);
+    });
   });
 }
