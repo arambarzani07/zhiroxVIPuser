f4558d4227de368838c85d9e871439e51f4667ae
arambarzani07 <arambarzani070@gmail.com>
2026-09-24 23:39:23 +0300

    fix(customers): keep controller tests strict-analyze clean

--- test/customer_directory_controller_test.dart
+++ test/customer_directory_controller_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:zhirox/features/customers/customer_directory_controller.dart';
